fcb3202cc88f7c9516d73a8c6e132e58b4ba1b64
Usama_Malik <usamamm510@gmail.com>
2023-06-17 21:35:58 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
         <Routes>
         <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
-          <Route path="/https://gleaming-douhua-6ed337.netlify.app/service" element={<Service />} />
+          <Route path="/service" element={<Service />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
       </div>
